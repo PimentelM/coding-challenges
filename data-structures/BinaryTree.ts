7b789f9945d96c882df44b595ee49c03b8c22220
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2021-10-07 12:04:50 -0300

    setRight/setLeft Binary tree

--- data-structures/BinaryTree.ts
+++ data-structures/BinaryTree.ts
@@ -1,130 +1,128 @@
-
 // Data structure written by Mateus Pimentel
 export class BinaryTreeNode {
-    parent: BinaryTreeNode;
-    value: any;
-    right: BinaryTreeNode;
-    left: BinaryTreeNode;
+  parent: BinaryTreeNode;
+  value: any;
+  right: BinaryTreeNode;
+  left: BinaryTreeNode;
 
-    private cache : any = {};
+  private cache: any = {};
 
-    constructor(value,parent = undefined){
-        this.parent = parent;
-        this.value = value;
+  constructor(value, parent = undefined) {
+    this.parent = parent;
+    this.value = value;
+  }
+
+  resetCache() {
+    this.cache = {};
+  }
+
+  setChildRight(child: BinaryTreeNode) {
+    this.right = child;
+  }
+
+  setChildLeft(child: BinaryTreeNode) {
+    this.left = child;
+  }
+
+  calcLevel(startingIndex: number = 1): number {
+    if (this.cache.level) return this.cache.level;
+    if (!this.parent) return startingIndex;
+
+    return (this.cache.level = this.parent.calcLevel() + 1);
+  }
+
+  inOrderTraverse(): BinaryTreeNode[] {
+    let result = [];
+
+    // Traverse left node
+    if (this.left) {
+      result.push(...this.left.inOrderTraverse());
     }
 
+    // Adds current value
+    result.push(this);
 
-    resetCache(){
-        this.cache = {}
+    // Traverse Right Node
+    if (this.right) {
+      result.push(...this.right.inOrderTraverse());
     }
 
-    calcLevel(startingIndex : number = 1) : number {
-        if(this.cache.level) return this.cache.level;
-        if(!this.parent) return startingIndex;
+    return result;
+  }
 
-        return this.cache.level = this.parent.calcLevel() + 1
+  preOrderTraverse(): BinaryTreeNode[] {
+    let result = [];
+
+    // Adds current value
+    result.push(this);
+
+    // Traverse left node
+    if (this.left) {
+      result.push(...this.left.preOrderTraverse());
     }
 
-    inOrderTraverse() : BinaryTreeNode[]{
-        let result = [];
-
-        // Traverse left node
-        if (this.left){
-            result.push(...this.left.inOrderTraverse())
-        }
-
-        // Adds current value
-        result.push(this)
-
-        // Traverse Right Node
-        if (this.right){
-            result.push(...this.right.inOrderTraverse())
-        }
-
-        return result
+    // Traverse Right Node
+    if (this.right) {
+      result.push(...this.right.preOrderTraverse());
     }
 
-    preOrderTraverse() : BinaryTreeNode[]{
-        let result = [];
+    return result;
+  }
 
-        // Adds current value
-        result.push(this)
+  postOrderTraverse(): BinaryTreeNode[] {
+    let result = [];
 
-        // Traverse left node
-        if (this.left){
-            result.push(...this.left.preOrderTraverse())
-        }
-
-        // Traverse Right Node
-        if (this.right){
-            result.push(...this.right.preOrderTraverse())
-        }
-
-        return result
+    // Traverse left node
+    if (this.left) {
+      result.push(...this.left.postOrderTraverse());
     }
 
-    postOrderTraverse() : BinaryTreeNode[]{
-        let result = [];
-
-        // Traverse left node
-        if (this.left){
-            result.push(...this.left.postOrderTraverse())
-        }
-
-        // Traverse Right Node
-        if (this.right){
-            result.push(...this.right.postOrderTraverse())
-        }
-
-        // Adds current value
-        result.push(this)
-
-        return result
+    // Traverse Right Node
+    if (this.right) {
+      result.push(...this.right.postOrderTraverse());
     }
 
-    executeForAllNodes(order : "pre" | "post" | "in", action : (node: BinaryTreeNode) => void){
-        this[`${order}OrderTraverse`]().map(action)
+    // Adds current value
+    result.push(this);
+
+    return result;
+  }
+
+  executeForAllNodes(order: 'pre' | 'post' | 'in', action: (node: BinaryTreeNode) => void) {
+    this[`${order}OrderTraverse`]().map(action);
+  }
+
+  executeForAllNodesInOrder(action: (node: BinaryTreeNode) => void) {
+    this.inOrderTraverse().map(action);
+  }
+
+  executeForAllNodesPreOrder(action: (node: BinaryTreeNode) => void) {
+    this.preOrderTraverse().map(action);
+  }
+
+  executeForAllNodesPostOrder(action: (node: BinaryTreeNode) => void) {
+    this.postOrderTraverse().map(action);
+  }
+
+  print(prefix = '', isTail = false) {
+    let nodeType = !this.parent ? `-` : this.parent.right === this ? `r` : `l`;
+
+    console.log(prefix + `|-${nodeType} ${this.value} {${this.calcLevel()}}`);
+
+    if (this.left) {
+      this.left.print(prefix + (isTail ? '    ' : '|   '), !this.right);
     }
 
-    executeForAllNodesInOrder(action : (node: BinaryTreeNode) => void){
-        this.inOrderTraverse().map(action)
+    if (this.right) {
+      this.right.print(prefix + (isTail ? '    ' : '|   '), true);
     }
 
-    executeForAllNodesPreOrder(action : (node: BinaryTreeNode) => void){
-        this.preOrderTraverse().map(action)
-    }
-
-    executeForAllNodesPostOrder(action : (node: BinaryTreeNode) => void){
-        this.postOrderTraverse().map(action)
-    }
-
-
-    print(prefix = "", isTail)
-    {
-        let nodeType =
-            !this.parent ? `-` :
-                (this.parent.right === this ? `r` : `l`)
-
-        console.log(prefix + `|-${nodeType} ${this.value} {${this.calcLevel()}}` )
-
-
-        if(this.left)
-        {
-            this.left.print(prefix + (isTail ? "    " : "|   "), !this.right )
-        }
-
-        if(this.right)
-        {
-            this.right.print(prefix + (isTail ?"    " : "|   "), true);
-        }
-
-        // If it's root
-        if(!this.parent)
-            console.log(
-                this.inOrderTraverse()
-                    .map(x=>x.value)
-                    .join(typeof this.value === "number" ? " " : "\n"))
-
-    }
-
+    // If it's root
+    if (!this.parent)
+      console.log(
+        this.inOrderTraverse()
+          .map(x => x.value)
+          .join(typeof this.value === 'number' ? ' ' : '\n')
+      );
+  }
 }
